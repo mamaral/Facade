5c0d780b95d45d1eed6c0b7f3c0eb3c430b742a1
Montak Oleg <montak.o@ya.ru>
2015-09-17 00:40:19 +0300

    Carthage compatibility

---
+++ Framework/Facade.h
@@ -0,0 +1,7 @@
+//! Project version number for Facade.
+FOUNDATION_EXPORT double FacadeVersionNumber;
+
+//! Project version string for Facade.
+FOUNDATION_EXPORT const unsigned char FacadeVersionString[];
+
+#import <Facade/UIView+Facade.h>
